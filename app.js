57175bbe8364604d590e01b92ca58947b895a3dc
cesar10riera-sys <cesar10riera@gmail.com>
2026-06-07 12:02:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
+// Helper functions to replace localStorage operations
+function guardarUsuarioFirebase(user) {
+  db.ref('usuarios/' + user.cedula).set(user);
+}
+function guardarReservaFirebase(cedula, reserva) {
+  db.ref('rancho/' + cedula).push().set(reserva);
+}
+function guardarPeluqueriaFirebase(cedula, turno) {
+  db.ref('peluqueria/' + cedula).push().set(turno);
+}
+// Replace all saveUsers(), saveTurns(), saveRanchoRecords() calls with these functions
+
+
+// ------------------ Firebase Initialization ------------------
+const firebaseConfig = {
+  apiKey: "AIzaSyA_2pPscem49DjtOqzCWY_fVtZ2npM-RG4",
+  authDomain: "cidfae-servicios.firebaseapp.com",
+  databaseURL: "https://cidfae-servicios-default-rtdb.firebaseio.com/",
+  projectId: "cidfae-servicios",
+  storageBucket: "cidfae-servicios.firebasestorage.app",
+  messagingSenderId: "199012208016",
+  appId: "1:199012208016:web:a676006c41761787637715",
+  measurementId: "G-KEMRD3GB2Z"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
 // Helper functions to save data to Firebase instead of localStorage
 function guardarUsuarioFirebase(user) {
   db.ref('usuarios/' + user.cedula).set(user);
